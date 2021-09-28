a966df8f582c5a5265724432a5953f7177249bcb
Uxbert Labs <uxbertlabs@Asgard.local>
2021-09-28 15:09:51 +0300

    1.0.3

--- pages/404.tsx
+++ pages/404.tsx
@@ -11,15 +11,6 @@ export default function Custom404() {
         404 - Page Not Found
       </h1>
       <br />
-      <div className="flex items-center justify-center">
-        <h1 className=" p-8 text-lg sm:text-2xl ">
-          Web page redirects after 2 seconds...
-          {/* <br /> if the page did not reload click{" "}
-          <Link href="/">
-            <a className="underline">here</a>
-          </Link> */}
-        </h1>
-      </div>
     </>
   );
 }
