9c89d3c5fc217d54b380fa4345690f4b97d7ca35
Acnologiax <zeref.sama@hotmail.com>
2021-10-07 03:25:35 +0300

    1.0.5

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
 import { server } from "../config/index";
-
 type MetaProps = {
   title: string;
   description: string;
@@ -28,7 +27,11 @@ const page = ({ meta, children }: PageProps) => {
         <meta property="og:site_name" content={"Shinigami no Akunorogia"} />
         <meta property="og:url" content={server} />
         <meta property="og:type" content="website" />
-        <meta name="image" property="og:image" content={"/og_image.png"} />
+        <meta
+          name="image"
+          property="og:image"
+          content={"https://i.imgur.com/j8TNfXD.png"}
+        />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
